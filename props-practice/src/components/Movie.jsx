9bf7fd29c8a33eaeabc599d1c91ee6a17ade994c
Kristiyan Radoslavov <radoslavov48@gmail.com>
2023-11-03 23:03:41 +0200

    commit new changes to the repo

--- props-practice/src/components/Movie.jsx
+++ props-practice/src/components/Movie.jsx
@@ -1,5 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export default function Movie(props) {
+    const [time, setTime] = useState(0);
+
+    setTimeout(() => {
+        setTime(time + 1);
+
+    }, 1000);
+
+
     return (
         <header>
             <h1>
@@ -8,6 +18,9 @@ export default function Movie(props) {
                 <li>{props.bookAuthor}</li>
             </h1>
 
+            <h2>
+                current time is {time}
+            </h2>
         </header>
     );
 }
